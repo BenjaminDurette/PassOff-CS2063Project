15f39becab727aac1e425403cebc0cf0eff9cb1b
B-Fanjoy <fanjoyben@hotmail.com>
2024-10-08 00:05:18 -0300

    Test demo ready
    Minimal function, inputs work, check phone files to see input

--- app/src/main/java/com/example/passoff/MainActivity.kt
+++ app/src/main/java/com/example/passoff/MainActivity.kt
@@ -1,47 +1,44 @@
 package com.example.passoff
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.passoff.ui.theme.PassOffTheme
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private var entryNameText: String? = ""
+    private var usernameText: String? = ""
+    private var passwordText: String? = ""
+    private var descriptionText: String? = ""
+    private var dbHandler: DBHandler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PassOffTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        dbHandler = DBHandler(this@MainActivity)
+
+        val addPasswordButton = findViewById<Button>(R.id.addpassword_button)
+        addPasswordButton.setOnClickListener {
+            entryNameText = findViewById<EditText>(R.id.entryname_input).text.toString()
+            usernameText = findViewById<EditText>(R.id.username_input).text.toString()
+            passwordText = findViewById<EditText>(R.id.password_input).text.toString()
+            descriptionText = findViewById<EditText>(R.id.description_input).text.toString()
+
+            if (entryNameText!!.isEmpty() || usernameText!!.isEmpty() || passwordText!!.isEmpty() || descriptionText!!.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Please fill all fields..", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+
+            this.dbHandler!!.addNewPassword(entryNameText, usernameText, passwordText, descriptionText)
+
+            Toast.makeText(this@MainActivity, "Password has been added.", Toast.LENGTH_SHORT).show()
+            findViewById<EditText>(R.id.entryname_input).setText("")
+            findViewById<EditText>(R.id.username_input).setText("")
+            findViewById<EditText>(R.id.password_input).setText("")
+            findViewById<EditText>(R.id.description_input).setText("")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PassOffTheme {
-        Greeting("Android")
     }
 }
